8b799e7aaee410698b18fe0f29e339d2e2935409
dzimiks <vana997@gmail.com>
2019-12-01 21:18:19 +0100

    Delete Data on the Server with fetch in React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {TodoForm, TodoList, Footer} from './components/todo';
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers';
 import {pipe, partial} from './lib/utils';
 import PropTypes from 'prop-types';
-import {loadTodos, createTodo, saveTodo} from './lib/todoService';
+import {loadTodos, createTodo, saveTodo, destroyTodo} from './lib/todoService';
 
 class App extends Component {
 	state = {
@@ -26,6 +26,8 @@ class App extends Component {
 		e.preventDefault();
 		const updatedTodos = removeTodo(this.state.todos, id);
 		this.setState({todos: updatedTodos});
+		destroyTodo(id)
+			.then(() => this.showTempMessage('Todo Removed'));
 	};
 
 	handleToggle = (id) => {
