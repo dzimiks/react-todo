b45148370c2f4d56560dc274510c656aaa44dd6a
dzimiks <vana997@gmail.com>
2019-12-01 18:42:41 +0100

    Build a Link Component to Navigate to Routes in React

---
+++ src/components/router/Link.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export class Link extends Component {
+	handleClick = (e) => {
+		e.preventDefault();
+		window.history.pushState(null, '', this.props.to);
+	};
+
+	render() {
+		return <a href="#" onClick={this.handleClick}>{this.props.children}</a>;
+	}
+}
+
+Link.propTypes = {
+	to: PropTypes.string.isRequired
+};
