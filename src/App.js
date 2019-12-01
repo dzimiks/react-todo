793fd314dd81e4950812c487798542aa08748f96
dzimiks <vana997@gmail.com>
2019-12-01 21:16:03 +0100

    Update Data on the Server with fetch in React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {TodoForm, TodoList, Footer} from './components/todo';
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers';
 import {pipe, partial} from './lib/utils';
 import PropTypes from 'prop-types';
-import {loadTodos, createTodo} from './lib/todoService';
+import {loadTodos, createTodo, saveTodo} from './lib/todoService';
 
 class App extends Component {
 	state = {
@@ -29,9 +29,13 @@ class App extends Component {
 	};
 
 	handleToggle = (id) => {
-		const getUpdatedTodos = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos));
-		const updatedTodos = getUpdatedTodos(id, this.state.todos);
+		const getToggledTodo = pipe(findById, toggleTodo);
+		const updated = getToggledTodo(id, this.state.todos);
+		const getUpdatedTodos = partial(updateTodo, this.state.todos);
+		const updatedTodos = getUpdatedTodos(updated);
 		this.setState({todos: updatedTodos});
+		saveTodo(updated)
+			.then(() => this.showTempMessage('Todo Updated'));
 	};
 
 	handleSubmit = (e) => {
