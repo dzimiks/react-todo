29dfe6938c658bee5901130b93f38a5b8767ced8
dzimiks <vana997@gmail.com>
2019-12-01 13:06:44 +0100

    Synchronize React UI and State with Controlled Components

--- src/App.js
+++ src/App.js
@@ -10,9 +10,16 @@ class App extends Component {
 				{id: 1, name: 'Learn JSX', isComplete: true},
 				{id: 2, name: 'Build an Awesome App', isComplete: false},
 				{id: 3, name: 'Ship It!', isComplete: false}
-			]
+			],
+			currentTodo: ''
 		};
 	}
+
+	handleInputChange = (e) => {
+		this.setState({
+			currentTodo: e.target.value
+		});
+	};
 
 	render() {
 		return (
@@ -23,7 +30,7 @@ class App extends Component {
 				</header>
 				<div className="Todo-App">
 					<form>
-						<input type="text"/>
+						<input type="text" onChange={this.handleInputChange} value={this.state.currentTodo}/>
 					</form>
 					<div className="Todo-List">
 						<ul>
