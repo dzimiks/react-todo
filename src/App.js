bf323ffbbfaa310575b4a6a086b3bc422c584dee
dzimiks <vana997@gmail.com>
2019-12-01 12:50:40 +0100

    Render a React UI with JSX

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo"/>
+				<h1>React Todos</h1>
+			</header>
+			<div className="Todo-App">
+				<form>
+					<input type="text"/>
+				</form>
+				<div className="Todo-List">
+					<ul>
+						<li><input type="checkbox"/> Learn JSX</li>
+						<li><input type="checkbox"/> Build an Awesome App</li>
+						<li><input type="checkbox"/> Ship It!</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
