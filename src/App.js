e816d0fff2e76567290815faf10b255b84ed152f
dzimiks <vana997@gmail.com>
2019-12-01 13:02:08 +0100

    Render a React UI Based on Initial State

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo"/>
-				<h1>React Todos</h1>
-			</header>
-			<div className="Todo-App">
-				<form>
-					<input type="text"/>
-				</form>
-				<div className="Todo-List">
-					<ul>
-						<li><input type="checkbox"/> Learn JSX</li>
-						<li><input type="checkbox"/> Build an Awesome App</li>
-						<li><input type="checkbox"/> Ship It!</li>
-					</ul>
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			todos: [
+				{id: 1, name: 'Learn JSX', isComplete: true},
+				{id: 2, name: 'Build an Awesome App', isComplete: false},
+				{id: 3, name: 'Ship It!', isComplete: false}
+			]
+		};
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo"/>
+					<h1>React Todos</h1>
+				</header>
+				<div className="Todo-App">
+					<form>
+						<input type="text"/>
+					</form>
+					<div className="Todo-List">
+						<ul>
+							{this.state.todos.map(todo =>
+								<li key={todo.id}>
+									<input type="checkbox" defaultChecked={todo.isComplete}/> {todo.name}
+								</li>)}
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
